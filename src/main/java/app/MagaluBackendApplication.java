7c3734f8b847fd53b7a1c54d4d21d9b8edf7abc6
calebavner <avner.trabalho@hotmail.com>
2024-08-14 05:25:06 -0300

    Inicializando projeto

---
+++ src/main/java/app/MagaluBackendApplication.java
@@ -0,0 +1,13 @@
+package app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagaluBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagaluBackendApplication.class, args);
+	}
+
+}
